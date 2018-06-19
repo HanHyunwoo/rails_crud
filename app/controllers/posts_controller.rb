66b470e8f311944989634e75080f2f18148ca3af
HOME <hhw1990@naver.com>
2018-06-19 16:26:48 +0900

    CRUD - U

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,11 @@ class PostsController < ApplicationController
 
   def create
     # 1.
-    Post.create(title: params[:title], body: params[:body])
+    post = Post.create(title: params[:title], body: params[:body])
+
+    # text 안에 변수를 넣을 때 (stringinterpolation)
+    # 반드시 ""로 해줘야 함.
+    redirect_to "/posts/#{post.id}"
 
     # # 1-1
     # Post.create(:title => params[:title], :body => params[:body])
@@ -19,4 +23,31 @@ class PostsController < ApplicationController
     # post.body = params[:body]
     # post.save
   end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+
+  def destroy
+    post = Post.find(params[:id])
+    post.destroy
+    redirect_to '/'
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def edit2
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    post = Post.find(params[:id])
+    post.update(title: params[:title], body: params[:body])
+    redirect_to '/'
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,17 @@ Rails.application.routes.draw do
   get 'posts/index'
 
   get 'posts/new'
-
   get 'posts/create'
-  get 'posts/show'
+  get 'posts/update' => 'posts#update'
+
+  get 'posts/:id' => 'posts#show'
+  get 'posts/:id/destroy' => 'posts#destroy'
+  get 'posts/:id/edit' => 'posts#edit'
+  get 'posts/:id/edit2' => 'posts#edit2'
+
+  get 'posts/:id/update' => 'posts#update'
+
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
